77730a6d2ee289e98749d577d818abc63f52891a
Daniel Eberli <eberlid@gmail.com>
2017-01-02 01:10:38 +0100

    add dealer tests

--- src/lib/BlackJack.hs
+++ src/lib/BlackJack.hs
@@ -1,6 +1,4 @@
-module BlackJack (play, Table, showTables) where
-
-import Data.List
+module BlackJack (play, getCards, Table, Deck, Hand(..), dealerAction, createDeck, showTables, Card(..), CardValue (Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace), Suit (Spade, Diamond, Club, Heart)) where
 
 data RoundResult = PlayerWin | PlayerBust | StandOff | PlayerLoose | PlayerBlackJack | Undefined
     deriving (Read, Show, Enum, Eq, Ord)
@@ -67,6 +65,13 @@ shortCardSuit suit
     | suit == Diamond = "D"
     | suit == Heart = "H"
     | otherwise = "x"
+
+-- |Returns the specified number of 52-card decks as on ordered list
+createDeck :: Int -> Deck
+createDeck deckCount =
+    if deckCount <= 0 || deckCount > 6
+    then []
+    else [Card val su | val <- [Two .. Ace], su <- [Club .. Spade]] ++ createDeck (deckCount - 1)
 
 shortCard :: Card -> String
 shortCard card = shortCardValue (getCardValue card) ++ shortCardSuit (getCardSuit card)
@@ -220,36 +225,11 @@ acesAsOneCountLoop cards n =
 boxCardCount :: Box -> Int
 boxCardCount box = sum (map (length . getCards . getPlayerHand) (getPlayerDecks box))
 
--- |Returns a shuffled deck with the specified number of 52-card decks
-staticShuffledDeck :: Int -> Deck
-staticShuffledDeck deckCount = shuffle (createDeck deckCount)
-
--- |Shuffles a deck with up to 312 cards
-shuffle :: Deck -> Deck
-shuffle deck =
-    map snd (sort (zip (filter (\r -> r < length deck) rand) deck))
-
--- |Returns randomized list of numbers [0..311] (6 decks à 52 cards)
-rand :: [Int]
-rand = [287, 202, 306, 170, 15, 125, 104, 261, 117, 305, 255, 301, 246, 78, 239, 79, 98, 281, 296, 45, 210, 273, 307, 227, 41, 10, 50, 149, 252, 8, 107, 11, 53, 217, 97, 90, 247, 74, 309, 212, 184, 95, 48, 153, 81, 262, 83, 203, 272, 63, 264, 75, 187, 302, 145, 110, 300, 31, 57, 185, 249, 55, 282, 199, 100, 173, 228, 230, 24, 267, 84, 128, 62, 148, 191, 303, 133, 297, 0, 205, 198, 193, 77, 279, 36, 269, 17, 118, 85, 152, 111, 89, 28, 38, 190, 208, 216, 22, 142, 87, 113, 58, 167, 169, 288, 52, 68, 26, 195, 124, 201, 204, 70, 260, 115, 280, 292, 139, 138, 233, 5, 134, 144, 64, 225, 130, 14, 49, 164, 241, 200, 259, 285, 243, 215, 213, 82, 23, 189, 33, 30, 291, 32, 235, 155, 207, 34, 265, 127, 294, 234, 289, 299, 276, 156, 157, 37, 242, 120, 238, 222, 275, 286, 76, 9, 165, 46, 160, 310, 158, 71, 27, 122, 268, 250, 209, 119, 88, 67, 180, 103, 132, 25, 54, 197, 93, 7, 244, 168, 3, 61, 99, 214, 283, 56, 256, 179, 35, 166, 143, 254, 263, 290, 129, 51, 220, 226, 60, 20, 223, 298, 101, 80, 181, 177, 12, 172, 91, 39, 271, 311, 1, 206, 140, 163, 69, 96, 43, 29, 266, 194, 150, 109, 131, 182, 102, 188, 284, 59, 295, 231, 47, 232, 42, 137, 221, 248, 65, 253, 154, 274, 293, 192, 66, 304, 105, 178, 147, 240, 114, 73, 308, 116, 13, 161, 162, 196, 258, 270, 245, 171, 106, 175, 218, 174, 224, 278, 19, 211, 251, 92, 135, 44, 146, 141, 40, 236, 2, 6, 237, 186, 126, 121, 123, 229, 21, 16, 136, 159, 94, 112, 4, 18, 86, 183, 151, 219, 72, 176, 257, 108, 277]
-
--- |Returns the specified number of 52-card decks as on ordered list
-createDeck :: Int -> Deck
-createDeck deckCount =
-    if deckCount <= 0 || deckCount > 6
-    then []
-    else [Card val su | val <- [Two .. Ace], su <- [Club .. Spade]] ++ createDeck (deckCount - 1)
-
 bet :: Float
 bet = 1.0
 
-numberOfDecks :: Int
-numberOfDecks = 6
-
-play :: Int -> [Table]
-play numberOfRounds = do 
-    -- let deck = [Card Nine Spade, Card Five Heart, Card Nine Diamond, Card Two Heart, Card Five Club, Card Ten Heart, Card King Spade, Card Five Diamond]
-    let deck = staticShuffledDeck numberOfDecks
+play :: Deck -> Int -> [Table]
+play deck numberOfRounds = do 
     playLoop deck numberOfRounds 0 0
 
 playLoop :: Deck -> Int -> Int -> Float -> [Table]
--- test/BlackJackSpec.hs
+++ test/BlackJackSpec.hs
@@ -1,11 +1,28 @@
 module BlackJackSpec (spec) where
 
-import BlackJack (play)
+import BlackJack
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = do
-   describe "blackjack" $ do
-       it "returns the first element on a list" $ do
-           head [23..] `shouldBe` (22 :: Int)
+    describe "dealer" $ do
+        it "stands on hard seventeen" $ do
+            getCards (dealerAction [] (Hand [Card Ten Club, Card Seven Spade] False False)) 
+                `shouldBe` ([Card Ten Club, Card Seven Spade])
+
+        it "hits on hard sixteen" $ do
+            getCards (dealerAction [Card Eight Club] (Hand [Card Ten Club, Card Six Spade] False False)) 
+                `shouldBe` ([Card Ten Club, Card Six Spade, Card Eight Club])
+
+        it "stands on soft seventeen" $ do
+            getCards (dealerAction [] (Hand [Card Ace Club, Card Six Spade] False False)) 
+                `shouldBe` ([Card Ace Club, Card Six Spade])
+
+        it "hits on soft sixteen" $ do
+            getCards (dealerAction [Card Four Club] (Hand [Card Ace Club, Card Five Spade] False False)) 
+                `shouldBe` ([Card Ace Club, Card Five Spade, Card Four Club])
+
+        it "hits on soft bust" $ do
+            getCards (dealerAction [Card Six Club, Card Two Heart, Card Three Diamond] (Hand [Card Ace Club, Card Five Spade] False False)) 
+                `shouldBe` ([Card Ace Club, Card Five Spade, Card Six Club, Card Two Heart, Card Three Diamond])
