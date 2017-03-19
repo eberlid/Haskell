34c9d284a3ff36b6a0617c46d7ada2be3fe3068f
Daniel Eberli <eberlid@gmail.com>
2017-03-19 08:30:07 +0100

    Execute Tests from Sublime with ctrl-b

--- test/BlackJackSpec.hs
+++ test/BlackJackSpec.hs
@@ -1,28 +1,36 @@
-module BlackJackSpec (spec) where
+module BlackJackSpec where
 
 import BlackJack
 import Test.Hspec
-import Test.Hspec.QuickCheck
+
+main :: IO ()
+main = hspec spec
 
 spec :: Spec
 spec = do
     describe "dealer" $ do
-        it "stands on hard seventeen" $ do
-            getCards (dealerAction [] (Hand [Card Ten Club, Card Seven Spade] False False)) 
-                `shouldBe` ([Card Ten Club, Card Seven Spade])
+        it "stands on hard seventeen" $ getCards
+            (dealerAction []
+                (Hand [Card Ten Club, Card Seven Spade] False False))
+            `shouldBe` [Card Ten Club, Card Seven Spade]
 
-        it "hits on hard sixteen" $ do
-            getCards (dealerAction [Card Eight Club] (Hand [Card Ten Club, Card Six Spade] False False)) 
-                `shouldBe` ([Card Ten Club, Card Six Spade, Card Eight Club])
+        it "hits on hard sixteen" $ getCards
+            (dealerAction [Card Eight Club]
+                (Hand [Card Ten Club, Card Six Spade] False False))
+            `shouldBe` [Card Ten Club, Card Six Spade, Card Eight Club]
 
-        it "stands on soft seventeen" $ do
-            getCards (dealerAction [] (Hand [Card Ace Club, Card Six Spade] False False)) 
-                `shouldBe` ([Card Ace Club, Card Six Spade])
+        it "stands on soft seventeen" $ getCards
+            (dealerAction []
+                (Hand [Card Ace Club, Card Six Spade] False False))
+            `shouldBe` [Card Ace Club, Card Six Spade]
 
-        it "hits on soft sixteen" $ do
-            getCards (dealerAction [Card Four Club] (Hand [Card Ace Club, Card Five Spade] False False)) 
-                `shouldBe` ([Card Ace Club, Card Five Spade, Card Four Club])
+        it "hits on soft sixteen" $ getCards
+            (dealerAction [Card Four Club]
+                (Hand [Card Ace Club, Card Five Spade] False False))
+            `shouldBe` [Card Ace Club, Card Five Spade, Card Four Club]
 
-        it "hits on soft bust" $ do
-            getCards (dealerAction [Card Six Club, Card Two Heart, Card Three Diamond] (Hand [Card Ace Club, Card Five Spade] False False)) 
-                `shouldBe` ([Card Ace Club, Card Five Spade, Card Six Club, Card Two Heart, Card Three Diamond])
+        it "hits on soft bust" $ getCards
+            (dealerAction [Card Six Club, Card Two Heart, Card Three Diamond]
+                (Hand [Card Ace Club, Card Five Spade] False False))
+            `shouldBe` [Card Ace Club, Card Five Spade, Card Six Club, Card Two Heart, Card Three Diamond]
+            
