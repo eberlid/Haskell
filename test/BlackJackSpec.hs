d9e963c02d4e788e132a5de5f8c5bfcfd2200990
Daniel Eberli <eberlid@gmail.com>
2017-03-20 23:36:02 +0100

    Add tests for Hand Value and Black Jack

--- test/BlackJackSpec.hs
+++ test/BlackJackSpec.hs
@@ -31,25 +31,56 @@ spec = describe "BlackJack" $ do
     
     describe "Split" $ do
         it "when equal value and split count is zero is allowed " $
-            canSplit (Hand [Card Six Club, Card Six Spade] False False) 0
+            canSplit (freshHand [Card Six Club, Card Six Spade]) 0
             `shouldBe` True
 
         it "when equal value and split count is one is allowed " $
-            canSplit (Hand [Card Six Club, Card Six Spade] False False) 1
+            canSplit (freshHand [Card Six Club, Card Six Spade]) 1
             `shouldBe` True
 
         it "when equal value and split count is two is allowed " $
-            canSplit (Hand [Card Six Club, Card Six Spade] False False) 2
+            canSplit (freshHand [Card Six Club, Card Six Spade]) 2
             `shouldBe` True
 
         it "when equal value and split count is three is not allowed " $
-            canSplit (Hand [Card Six Club, Card Six Spade] False False) 3
+            canSplit (freshHand [Card Six Club, Card Six Spade]) 3
             `shouldBe` False
 
         it "when not equal value and split count is zero is not allowed " $
-            canSplit (Hand [Card Six Club, Card Ten Spade] False False) 0
+            canSplit (freshHand [Card Six Club, Card Ten Spade]) 0
             `shouldBe` False
 
         it "when not equal value and split count is three is not allowed " $
-            canSplit (Hand [Card Six Club, Card Ten Spade] False False) 3
+            canSplit (freshHand [Card Six Club, Card Ten Spade]) 3
             `shouldBe` False
+
+    describe "Double" $ do
+        it "when card count is two then double is allowed" $
+            canDouble (freshHand [Card Two Club, Card Ten Spade])
+            `shouldBe` True
+
+        it "when card count is not two then double is not allowed" $
+            canDouble (freshHand [Card Two Club, Card Ten Spade, Card Five Heart])
+            `shouldBe` False
+
+    describe "Hand Value" $ do
+        it "when fresh hand then ace counts 11 on soft value" $
+            handValue (freshHand [Card Ace Club, Card Three Spade])
+            `shouldBe` 14
+
+        it "when fresh hand then ace counts 1 on hard value" $
+            handValue (Hand [Card Ace Club, Card Three Spade, Card Nine Diamond] False False)
+            `shouldBe` 13
+
+        it "when doubled hand then ace counts 11" $
+            handValue (Hand [Card Ace Club, Card Three Spade, Card Nine Diamond] False True)
+            `shouldBe` 23
+
+    describe "Black Jack" $ do
+        it "when splitted hand then is not black jack" $
+            isBlackJack (Hand [Card Ten Club, Card Ace Diamond] True False)
+            `shouldBe` False
+
+        it "when fresh hand then is black jack" $
+            isBlackJack (Hand [Card Ten Club, Card Ace Diamond] False False)
+            `shouldBe` True
